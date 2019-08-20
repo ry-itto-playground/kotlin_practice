152a80a59ad44f0a4e75e2bffc1cef9e5a1b5987
ryoya_ito <horusu.dqx@gmail.com>
2019-08-20 23:59:35 +0900

    FunctionalProgramming Lambda

--- src/tutorials/FunctionalProgramming.kt
+++ src/tutorials/FunctionalProgramming.kt
@@ -11,6 +11,11 @@ class Divider: (Int, Int) -> Double {
     override fun invoke(p1: Int, p2: Int): Double = p1.toDouble() / p2
 }
 
+// 関数の最後のパラメータの場合, パラメータの宣言をしているかっこの外側にラムダを書くことができる
+fun sample(init: Int, function: (Int, Int) -> Int) {
+    println(init + function(10, 2))
+}
+
 fun main() {
     // 関数を格納
     val f: (Int, Int) -> Double = ::safeDivide
@@ -18,4 +23,15 @@ fun main() {
     println(quotient)
     val div = Divider()
     println(div(4, 2))
+    // ラムダ式
+    val safeDivide = { numerator: Int, denominator: Int -> Double
+        if (denominator == 0) 0.0 else numerator.toDouble() / denominator
+    }
+    println(safeDivide(3, 0))
+    // 1つしかパラメータのない場合のラムダ. Swiftの$0みたいな
+    val square: (Double) -> Double = { it * it }
+    println(square(2))
+    sample(2) { x: Int, y: Int ->
+        x * y
+    }
 }
