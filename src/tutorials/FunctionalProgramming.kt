60bf6db3ef7dfef2efb7f0982e6573b04ecb6ee3
ryoya_ito <horusu.dqx@gmail.com>
2019-08-22 00:08:02 +0900

    FunctionalProgramming

--- src/tutorials/FunctionalProgramming.kt
+++ src/tutorials/FunctionalProgramming.kt
@@ -1,5 +1,7 @@
 package tutorials
 
+import java.time.Instant
+
 /**
  * https://kotlinlang.org/docs/tutorials/kotlin-for-py/functional-programming.html
  */
@@ -14,6 +16,26 @@ class Divider: (Int, Int) -> Double {
 // 関数の最後のパラメータの場合, パラメータの宣言をしているかっこの外側にラムダを書くことができる
 fun sample(init: Int, function: (Int, Int) -> Int) {
     println(init + function(10, 2))
+}
+
+class A {
+    val value = "AAAAA"
+    inner class B {
+        val value = "BBBBB"
+        // 暗黙的にクラスBのthisになる
+        val x = this
+        // 明示的にクラスBのthis
+        val y = this@B
+        // クラスAのthisをとる
+        val z = this@A
+    }
+}
+
+// 呼び出した時に関数内容が展開されるような関数を定義. 主に引数に関数を受けるときに使う。引数に与えられた関数の前後に処理を挟むとか。
+inline fun time(f: () -> Unit): Long {
+    val start = Instant.now().toEpochMilli()
+    f()
+    return Instant.now().toEpochMilli() - start
 }
 
 fun main() {
@@ -34,4 +56,32 @@ fun main() {
     sample(2) { x: Int, y: Int ->
         x * y
     }
+
+    // レシーバを指定した拡張関数の宣言
+    val boast: Divider.() -> String = {"Divider!!!"}
+    // レシーバを指定したラムダ内のthisはレシーバ
+    val hoge: Divider.() -> String = {"${this.invoke(1, 2)}"}
+    println(div.boast())
+    println(div.hoge())
+    val b = A().B()
+    println(b.x.value)
+    println(b.y.value)
+    println(b.z.value)
+    val t = time { println("hoge") }
+    println(t)
+
+    var n: String? = null
+    println(n?.length) // => null
+    println(n?.run { println("receive null value!!") }) // => null
+    n = "hoge"
+    println(n?.run {
+        println(this) // => hoge
+    }) // => kotlin.Unit
+
+    // run:let, apply:alsoの使い分けの記事。結構直感的でわかりやすかった
+    // https://qiita.com/JohnSmithWithHaruhi/items/e8f411c379483d4902aa
+
+    // takeIf
+    n?.takeIf { !it.isEmpty() }.run { println(this) }
+    n?.takeUnless { it.isEmpty() }.run { println(this) }
 }
